fc48cdb88497cf39af09efd95d06365b241c63fb
MarcoCicognani <marco.cicognani@btstore.com>
2018-10-18 09:00:04 +0200

    GUIIIISSS

--- XmlLibrary/MainWindow.xaml.cs
+++ XmlLibrary/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace XmlLibrary
         {
             InitializeComponent();
         }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
