898b89a5ea7dd6e0dd8c08ca3afeb743dba77e01
MarcoCicognani <marco.cicognani@btstore.com>
2018-10-22 08:20:58 +0200

    GUI work

--- XmlLibrary/MainWindow.xaml.cs
+++ XmlLibrary/MainWindow.xaml.cs
@@ -13,28 +13,39 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
-using System.Text;
+using System.Timers;
 
 namespace XmlLibrary
 {
     /// <summary>
     /// Logica di interazione per MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        // the timer used when need to put an 
+        // "asyncronous" message on a label, like an error
+        private Timer timer;
+
+        /**
+         * Main window constructor
+         */
+        public MainWindow() {
+            // initializate the componets
             InitializeComponent();
+            // create the timer
+            timer = new Timer(1500);
         }
 
-        private void btnConvert_Click(object sender, RoutedEventArgs e)
-        {
+        /**
+         * Button action method
+         */
+        private void btnConvert_Click(object sender, RoutedEventArgs e) {
             // get the text into the two textboxes
             if (String.IsNullOrEmpty(txtFrom.Text) || String.IsNullOrEmpty(txtTo.Text)) {
                 // show outsise a message to warn the user
                 var label = (txtFrom.Text == String.Empty ? txtFrom : txtTo);
-
-                MessageBox.Show("Missing " + (String.IsNullOrEmpty(txtFrom.Text) ? "From" : "To") + " path");
+                label.Foreground = Brushes.Red;
+                string old = label.Text;
+                label.Text = "Missing path";
                 return;
             }
 
